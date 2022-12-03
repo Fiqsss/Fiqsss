241c17b9813b9823717630ca31504517a1a940b0
Fiqsss <sarofiqs@gmail.com>
2022-12-04 01:21:32 +0700

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","","tokokelompok3");
+
+if(!$conn){
+	echo "gagal konek database menn";
+} else {
+	
+};
+
+?>
